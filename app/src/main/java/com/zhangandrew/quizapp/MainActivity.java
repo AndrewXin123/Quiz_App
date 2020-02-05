6e967fde254932ff2525b65517eb91ec0894a880
AndrewXin123 <AndrewXin12345@gmail.com>
2020-02-05 15:10:45 -0500

    Done

--- app/src/main/java/com/zhangandrew/quizapp/MainActivity.java
+++ app/src/main/java/com/zhangandrew/quizapp/MainActivity.java
@@ -5,19 +5,38 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button submitButton;
+    TextView displayText;
+    EditText responseText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         submitButton = findViewById(R.id.clickButton);
+        displayText=findViewById(R.id.clickButton);
+        responseText=findViewById(R.id.responseEditText);
         submitButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 System.out.println("Hello mom");
-                Log.i("testButton", "Hi dad!");
+                Log.i("testButton", "Hi dad!" + responseText.getText());
+                displayText.setText(displayText.getText() + " " + responseText.getText());
+            }
+        });
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    if(responseText.getText().toString().equals("TJ")){
+                        displayText.setText("TJ Rocks!");
+                        responseText.setText("");
+                        responseText.setHint("That's a good name.");
+                    }
+                }
             }
         });
     }
